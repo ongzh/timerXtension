6d362ac07c182d5961376f95ee2b11fd62dbf2f5
Ong <ong2323@gmail.com>
2023-03-16 14:13:45 -0400

    notifications feature

--- options.js
+++ options.js
@@ -1,16 +1,17 @@
 const nameInput = document.getElementById("name-input")
+const timeInput = document.getElementById("time-input")
 const saveBtn = document.getElementById("save-btn")
 
 saveBtn.addEventListener("click", () => {
     const name = nameInput.value
+    const notificationTime = timeInput.value
     chrome.storage.sync.set({
       name,
-    },()=>{
-        console.log(`Name is set to ${name}`)
+      notificationTime,
     })
   })
 
-  chrome.storage.sync.get(["name", "test"], (res)=>{
+  chrome.storage.sync.get(["name", "notificationTime"], (res)=>{
     nameInput.value = res.name ?? "???"
-
+    timeInput.value = res.notificationTime ?? 1000
   })
