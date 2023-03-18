b81e46b7f14051b5a4823ca19eeff39c2663604c
Anthony Ong <ongzh23@gmail.com>
2023-03-17 22:56:18 -0400

    rest time function

--- background.js
+++ background.js
@@ -2,33 +2,56 @@ chrome.alarms.create("pomodoroTimer", {
     periodInMinutes: 1 / 60,
 })
 
-chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res)=>{
+chrome.storage.local.get(["timer", "isRunning", "timeOption", "restOption"], (res)=>{
     chrome.storage.local.set({
         timer:"timer" in res ? res.timer:0,
         isRunning: "isRunning" in res ? res.isRunning: false,
-        timeOption: "timeOption" in res ? res.timeOption : 25 
+        isResting: "isResting" in res ? res.isResting: false,
+        timeOption: "timeOption" in res ? res.timeOption : 25,
+        restOption: "restOption" in res ? res.restOption: 5
+
     })
 })
 
 
 chrome.alarms.onAlarm.addListener((alarm)=>{
     if (alarm.name === "pomodoroTimer"){
-        chrome.storage.local.get(["timer", "isRunning"], (res)=>{
+        chrome.storage.local.get(["timer", "isRunning", "isResting", "restOption","timeOption"], (res)=>{
+            
+            if (res.isResting){
+                let timer = res.timer + 1 
+                let isResting = true 
+                if (timer == 60 * res.restOption){
+                    this.registration.showNotification("Pomodoro Timer",
+                    {
+                        body: `${res.restOption} minutes has passed! Get back to work!`,
+                        icon: "icon.png"
+                    })
+                    timer = 0
+                    isRunning = false
+                    isResting = false 
+                }
+            }
+            
             if (res.isRunning) {
                 let timer = res.timer + 1
                 let isRunning = true
                 if (timer == 60 * res.timeOption){
                     this.registration.showNotification("Pomodoro Timer",
                     {
-                        body: `${res.timeOption} minutes has passed!`,
+                        body: `${res.timeOption} minutes has passed! Rest time starts now!`,
                         icon: "icon.png"
                     })
                     timer = 0
-                    isRunning = false 
+                    isRunning = false
+                    if (res.restOption != 0){
+                        isResting = true 
+                    }
                 }
                 chrome.storage.local.set({
                     timer,
-                    isRunning
+                    isRunning,
+                    isResting: res.isResting,
                 })
                 chrome.action.setBadgeText({
                     text: `${timer}`
--- options/options.js
+++ options/options.js
@@ -1,5 +1,14 @@
 const saveBtn = document.getElementById("save-btn")
-const timeOption = document.getElementById("timer-input")
+const timeOption = document.getElementById("focus-input")
+const restOption = document.getElementById("rest-input")
+
+
+restOption.addEventListener("change", (event)=>{
+  const val = event.target.value
+  if (val<0 || val>60){
+    timeOption.value = 5
+  }
+})
 
 timeOption.addEventListener("change", (event)=>{
   const val = event.target.value
@@ -8,15 +17,17 @@ timeOption.addEventListener("change", (event)=>{
   }
 })
 
-
 saveBtn.addEventListener("click", () => {
     chrome.storage.local.set({
       timer: 0,
       timeOption: timeOption.value,
       isRunning: false,
+      isResting: false,
+      restOption: restOption.value,
     })
   })
 
-chrome.storage.local.get(["timeOption"], (res)=>{
+chrome.storage.local.get(["timeOption", "restOption"], (res)=>{
   timeOption.value = res.timeOption
+  restOption.value = res.restOption
 })
